ad05ec908c273b33b503f713f0d7f975ab598160
Soobin Kim <sbin1.kim@navercorp.com>
2022-04-27 14:57:18 +0900

    initial commit

---
+++ src/main/java/com/sbstudy/configexample2/ConfigExample2Application.java
@@ -0,0 +1,13 @@
+package com.sbstudy.configexample2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigExample2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigExample2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sbstudy/configexample2/ConfigExample2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbstudy.configexample2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigExample2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
