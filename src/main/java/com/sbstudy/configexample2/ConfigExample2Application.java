d87bbba3efeeef131e1c210be11369b988040965
Soobin Kim <sbin1.kim@navercorp.com>
2022-04-27 15:06:03 +0900

    ready

--- src/main/java/com/sbstudy/configexample2/ConfigExample2Application.java
+++ src/main/java/com/sbstudy/configexample2/ConfigExample2Application.java
@@ -2,8 +2,10 @@ package com.sbstudy.configexample2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigExample2Application {
 
 	public static void main(String[] args) {
